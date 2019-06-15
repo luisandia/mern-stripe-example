bdf228fda1af1bc3a84e777b2ef8eb51fac4ba6a
luisandia <luisandiamamani@gmail.com>
2019-06-15 01:28:53 +0000

    add reducers

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- server/services/passport.js
+++ server/services/passport.js
@@ -13,22 +13,19 @@ passport.deserializeUser((id, done) => {
     User.findById(id).then((user) => {
         done(null, user);
     });
-}
-)
+})
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     // proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-        .then((existingUser) => {
-            if (existingUser) {
-                done(null, existingUser);
-            } else {
-                new User({ 'googleId': profile.id }).save().then(user => done(null, user));
-            }
-        });
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id })
+    if (existingUser) {
+        return done(null, existingUser);
+    }
+    const user = new User({ 'googleId': profile.id }).save();
+    done(null, user);
 }));
 
